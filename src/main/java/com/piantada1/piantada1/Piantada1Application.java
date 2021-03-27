3bb5b59dee329f5f7d987618fb7ccf32daa57ff2
vieradaniel <dariennx@hotmail.com>
2021-03-27 18:34:46 -0300

    first commit

---
+++ src/main/java/com/piantada1/piantada1/Piantada1Application.java
@@ -0,0 +1,13 @@
+package com.piantada1.piantada1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Piantada1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Piantada1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/piantada1/piantada1/Piantada1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piantada1.piantada1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Piantada1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
